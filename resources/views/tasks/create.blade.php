64dfece967b0eb556efbd991ec039be113326e9f
Gurkirat Singh <tbhaxor@gmail.com>
2024-05-14 10:47:46 +0530

    Add `is-error` class on error in the fields

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -9,7 +9,7 @@
         @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="title" class="form-control error" id="title" name="title" placeholder="Enter the task title....">
+                <input type="title" class="form-control @error('title') is-invalid @enderror" required id="title" name="title" placeholder="Enter the task title....">
                 @error('title')
                     <div class="form-text text-danger">{{ $message }}</div>
                 @enderror
