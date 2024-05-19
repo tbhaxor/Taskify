00ab0766bdbc89723641542875ed9865ae25051d
Gurkirat Singh <tbhaxor@gmail.com>
2024-05-19 14:24:55 +0530

    Fix test logic for `test_should_redirect_to_login_page_as_unauthenticated` and add authenticated user logout test

--- tests/Feature/Http/Controllers/Auth/LogoutControllerTest.php
+++ tests/Feature/Http/Controllers/Auth/LogoutControllerTest.php
@@ -2,13 +2,20 @@
 
 namespace Tests\Feature\Http\Controllers\Auth;
 
+use App\Models\User;
 use Tests\TestCase;
 
 class LogoutControllerTest extends TestCase
 {
-    public function test_should_redirect_to_login_page(): void
+    public function test_should_redirect_to_login_page_as_unauthenticated(): void
     {
-        $response = $this->post(route('auth.logout'));
+        $response = $this->get(route('auth.logout'));
+        $response->assertRedirectToRoute('auth.login');
+    }
+
+    public function test_should_redirect_to_login_page_as_authenticated(): void
+    {
+        $response = $this->actingAs(User::all()->first())->get(route('auth.logout'));
         $response->assertRedirectToRoute('auth.login');
     }
 }
