e91e1e19b55148e01106973c312b7b281d7f1394
Gurkirat Singh <tbhaxor@gmail.com>
2024-06-08 05:02:48 +0530

    TestHelper: Add user related methods and adapt to new workflow

--- tests/Traits/TestHelper.php
+++ tests/Traits/TestHelper.php
@@ -4,37 +4,48 @@ namespace Tests\Traits;
 
 use App\Models\Group;
 use App\Models\Task;
+use App\Models\User;
 use Illuminate\Support\Collection;
 
 trait TestHelper
 {
-    private function createGroup(): Group
+    /**
+     * @param array $attributes Override the default factory definitions
+     */
+    private function createGroup(array $attributes = []): Group
     {
-        return Group::factory()->create();
+        $group = Group::factory()->create($attributes);
+
+        // For some reason this is returned null but the user record exists
+        // FIXME: This shouldn't be the case, investigation is required
+        if (is_null($group->user)) {
+            $group->user = User::findOrFail($group->user_id);
+        }
+        return $group;
     }
 
     /**
+     * @param array $attributes Override the default factory definitions
      * @param int $count
      * @return Collection<int, Group>
      */
-    private function createGroups(int $count = 2): Collection
+    private function createGroups(int $count = 2, array $attributes = []): Collection
     {
-        return Group::factory($count)->count($count)->create();
+        return Group::factory($count)->count($count)->create($attributes);
     }
 
-    /**
-     * @param bool $associateWithGroup
-     * @return Task
-     */
-    private function createTask(bool $associateWithGroup = true): Task
+    private function createTask(): Task
     {
-        if ($associateWithGroup) {
-            return Task::factory()->create([
-                'group_id' => $this->createGroup()->id
-            ]);
-        }
-
         return Task::factory()->create();
+    }
+
+
+    /**
+     * @param array $attributes Override the default factory definitions
+     */
+    private function createUser(array $attributes = []): User
+    {
+        return User::factory()->create($attributes);
     }
 
 }
