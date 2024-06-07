ff6062ca3f29bb732a27d623e9fa9fc1e60fe9c4
Gurkirat Singh <tbhaxor@gmail.com>
2024-06-08 05:04:00 +0530

    Models/Group: Add custom typing for $user magic field

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property User $user
+ */
 class Group extends Model
 {
     use HasFactory;
