6bc7f54983647b18275ce9ef9d1bf87ec6e4a905
Gurkirat Singh <tbhaxor@gmail.com>
2024-06-08 07:09:01 +0530

    routes/auth: Refactor file and add reset/forgot password routes

--- routes/auth.php
+++ routes/auth.php
@@ -1,15 +1,25 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\Auth\SignupController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::match(['GET', 'POST'], '/auth/login', LoginController::class)->name('auth.login');
-    Route::match(['GET', 'POST'], '/auth/signup', SignupController::class)->name('auth.signup');
+
+Route::name('auth.')->prefix('auth')->group(function () {
+    Route::get('logout', LogoutController::class)->name('logout')->middleware('auth');
+
+    Route::middleware('guest')->group(function () {
+        Route::match(['GET', 'POST'], 'login', LoginController::class)->name('login');
+        Route::match(['GET', 'POST'], 'signup', SignupController::class)->name('signup');
+
+        Route::prefix('password')->name('password.')->group(function () {
+            Route::match(['GET', 'POST'], 'forgot', ForgotPasswordController::class)->name('forgot');
+            Route::match(['GET', 'POST'], 'reset', ResetPasswordController::class)->name('reset');
+        });
+    });
+
 });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/auth/logout', LogoutController::class)->name('auth.logout');
-});
