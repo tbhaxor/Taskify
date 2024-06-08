ee23a1fcc7a3a439345a5fef4fbb0f5ee88bde67
Gurkirat Singh <tbhaxor@gmail.com>
2024-06-08 07:09:31 +0530

    LoginController: Fix the remember token placement

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,20 +4,23 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
     /**
      * Handle the incoming request.
+     * @throws ValidationException
      */
-    public function __invoke(LoginRequest $request)
+    public function __invoke(LoginRequest $request): View|RedirectResponse
     {
         if ($request->isMethod('GET')) {
             return view('auth.login');
         }
 
-        if (auth()->attempt($request->safe(['email', 'password'], $request->boolean('remember')))) {
+        if (auth()->attempt($request->safe(['email', 'password']), $request->boolean('remember'))) {
             $request->session()->regenerate();
             return redirect()->intended();
         }
