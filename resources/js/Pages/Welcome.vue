5f8483a58cdaa64ffdc4cfc48d3e314556562439
Gurkirat Singh <tbhaxor@gmail.com>
2024-06-10 23:23:37 +0530

    Pages: Add Welcome page

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head } from "@inertiajs/vue3"
+</script>
+
+<template>
+    <Head title="Welcome Page"/>
+    <h1>Hello World</h1>
+</template>
+
+<style scoped>
+
+</style>
