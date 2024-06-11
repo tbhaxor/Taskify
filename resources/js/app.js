5e36bd59be62ee6c090423dc70beeb9a373da4e1
manjumjn <manjumjng2@gmail.com>
2024-06-12 00:58:12 +0530

    feat: Added tailwindcss and configured prettier with auto sorting classes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,16 @@
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import "../css/app.css";
 
 // noinspection JSIgnoredPromiseFromCall
 createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', {eager: true})
-        return pages[`./Pages/${name}.vue`]
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+        return pages[`./Pages/${name}.vue`];
     },
-    setup({el, App, props, plugin}) {
-        createApp({render: () => h(App, props)})
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
             .use(plugin)
-            .mount(el)
+            .mount(el);
     },
-})
+});
