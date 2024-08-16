3690157d233ee01e179ed5a8b934f1c18006d319
ĐARK々MÁTTER <tbhaxor@gmail.com>
2024-08-16 06:47:56 +0530

    Auth: Remove unwanted controllers and requests

--- routes/auth.php
+++ routes/auth.php
@@ -1,25 +1,13 @@
 <?php
 
-use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\CallbackController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\Auth\ResetPasswordController;
-use App\Http\Controllers\Auth\SignupController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::name('auth.')->prefix('auth')->group(function () {
-    Route::get('logout', LogoutController::class)->name('logout')->middleware('auth');
-
-    Route::middleware('guest')->group(function () {
-        Route::match(['GET', 'POST'], 'login', LoginController::class)->name('login');
-        Route::match(['GET', 'POST'], 'signup', SignupController::class)->name('signup');
-
-        Route::prefix('password')->name('password.')->group(function () {
-            Route::match(['GET', 'POST'], 'forgot', ForgotPasswordController::class)->name('forgot');
-            Route::match(['GET', 'POST'], 'reset', ResetPasswordController::class)->name('reset');
-        });
-    });
-
+    Route::get('login', LoginController::class)->name('login');
+    Route::get('logout', LogoutController::class)->name('logout');
+    Route::get('callback', CallbackController::class)->name('callback');
 });
-
