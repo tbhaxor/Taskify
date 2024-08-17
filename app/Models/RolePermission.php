ea8e9ffe609767a4371700c94e8b6fecdbe9b95a
ĐARK々MÁTTER <tbhaxor@gmail.com>
2024-08-17 19:10:32 +0530

    Models: Create table for Role and Permission
    RolePermission model is the repository for M:N relation between them.

---
+++ app/Models/RolePermission.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @mixin IdeHelperRolePermission
+ */
+class RolePermission extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+    protected $primaryKey = false;
+    public $incrementing = false;
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function permission(): BelongsTo
+    {
+        return $this->belongsTo(Permission::class);
+    }
+}
