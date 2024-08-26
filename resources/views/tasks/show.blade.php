0bcf96466e6b91e1c212fc86518de870fe2497c2
Gurkirst Singh <tbhaxor@gmail.com>
2024-08-27 01:53:49 +0530

    views: Reformat + Show controls based on authorization

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,3 +1,5 @@
+@use('App\Enums\UserPermission')
+
 @extends('layouts.base')
 
 @section('title', 'Show Task')
@@ -14,29 +16,34 @@
 @endsection
 
 @section('content')
-<div class="card">
-    <div class="card-body">
-        <h3 class="card-title">{{ $task->title }}</h3>
-        <pre class="card-text">{{ $task->description }}</pre>
-        <p><strong>Status:</strong> {{ $task->status }}</p>
-        @if ($task->status == \App\Enums\TaskStatus::Completed)
+    <div class="card">
+        <div class="card-body">
+            <h3 class="card-title">{{ $task->title }}</h3>
+            <pre class="card-text">{{ $task->description }}</pre>
+            <p><strong>Status:</strong> {{ $task->status }}</p>
+            @if ($task->status == \App\Enums\TaskStatus::Completed)
+                <p>
+                    <strong>Completed At:</strong>
+                    {{ $task->completed_at }}
+                </p>
+            @endif
             <p>
-                <strong>Completed At:</strong> 
-                {{ $task->completed_at }}
+                <strong>Created At:</strong>
+                {{ $task->created_at }}
             </p>
-        @endif
-        <p>
-            <strong>Created At:</strong>
-            {{ $task->created_at }}
-        </p>
-        <p>
-            <strong>Updated At:</strong>
-            {{ $task->updated_at }}
-        </p>
-      
-        <a href="{{ route('group.show', ['group' => $group]) }}" class="card-link">All Tasks</a>
-        <a href="{{ route('task.edit', ['task' => $task, 'group' => $group]) }}" class="card-link">Edit</a>
-        <a href="{{ route('task.delete', ['task' => $task, 'group' => $group]) }}" class="card-link text-danger">Delete</a>
+            <p>
+                <strong>Updated At:</strong>
+                {{ $task->updated_at }}
+            </p>
+
+            <a href="{{ route('group.show', ['group' => $group]) }}" class="card-link">All Tasks</a>
+            @can(UserPermission::EDIT_TASKS->value, $group)
+                <a href="{{ route('task.edit', ['task' => $task, 'group' => $group]) }}" class="card-link">Edit</a>
+            @endcan
+            @can(UserPermission::DELETE_TASKS->value, $group)
+                <a href="{{ route('task.delete', ['task' => $task, 'group' => $group]) }}"
+                   class="card-link text-danger">Delete</a>
+            @endcan
+        </div>
     </div>
-  </div>
 @endsection
