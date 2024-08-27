20279d715e5f01eeddf71590720a39aac2281431
Gurkirst Singh <tbhaxor@gmail.com>
2024-08-28 01:24:49 +0530

    CallbackController: Associate user with group in `UserGroupRole` if recently created
    Once the user is associated with group, it will then delete the user invite.

--- app/Http/Controllers/Auth/CallbackController.php
+++ app/Http/Controllers/Auth/CallbackController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\UserGroupRole;
+use App\Models\UserInvite;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -21,6 +23,19 @@ class CallbackController extends Controller
 
         /** @var User $user */
         $user = User::updateOrCreate(['email' => $profile->email], ['name' => $profile->name]);
+
+        if ($user->wasRecentlyCreated) {
+            UserInvite::whereEmail($user->email)
+                ->get()
+                ->each(function (UserInvite $invite) use ($user) {
+                    UserGroupRole::create([
+                        'user_id' => $user->id,
+                        'group_id' => $invite->group_id,
+                        'role_id' => $invite->role_id
+                    ]);
+                    $invite->delete();
+                });
+        }
 
         $request->session()->put('zitadel_id_token', $profile->accessTokenResponseBody['id_token']);
 
