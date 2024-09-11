d0bffb348adb29d2cf59195a888a83e7e3268a0e
Gurkirst Singh <tbhaxor@gmail.com>
2024-09-11 23:18:17 +0530

    AppServiceProvider: Enforce https scheme for URLs in non-local environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,6 +10,7 @@ use App\Events\UserInvite\DeleteUserInviteEvent;
 use App\Listeners\GroupSharingListener;
 use App\Listeners\UserInviteListener;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use SocialiteProviders\Manager\SocialiteWasCalled;
 
@@ -34,6 +35,10 @@ class AppServiceProvider extends ServiceProvider
         Event::listen(function (SocialiteWasCalled $socialiteWasCalled) {
             $socialiteWasCalled->extendSocialite('zitadel', \SocialiteProviders\Zitadel\Provider::class);
         });
+
+        if (!$this->app->isLocal()) {
+            URL::forceScheme('https');
+        }
 
         Event::listen(CreateUserInviteEvent::class, [UserInviteListener::class, 'handleCreated']);
         Event::listen(DeleteUserInviteEvent::class, [UserInviteListener::class, 'handleDeleted']);
