b0b0b4afd57ccc45ee3444476819e3bbb5518633
Gurkirst Singh <tbhaxor@gmail.com>
2024-09-12 00:24:58 +0530

    migration + entrypoint: Force `db:seed` on production

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 set -ex
 
 php artisan migrate --force
-php artisan db:seed
+php artisan db:seed --force
 chown taskify:taskify database/database.sqlite
 php artisan optimize
 /usr/bin/supervisord -c /etc/supervisord.conf
